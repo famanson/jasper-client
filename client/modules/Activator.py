819e60f22924c16080faa239e637fd1cd69e8e31
Son Pham <me@famanson.com>
2014-08-14 00:24:58 +0100

    Shutdown script

--- client/modules/Activator.py
+++ client/modules/Activator.py
@@ -1,4 +1,5 @@
 from awake import wol
+from subprocess import call
 import re
 
 
@@ -18,19 +19,32 @@ def handle(text, mic, profile):
 
     text = text.lower()
     ## Try to work out which OS to wake up
-    match = re.search(r'activate\ (?P<target>\ubuntu|fedora|linux|windows)', text)
+    match = re.search(r"(?P<action>\w+) (?P<target>\w+)", text)
     if match:
-        ## tempted to just use 'os' here but it is a package name, meh
+        ## tempted to just use "os" here but it is a package name, meh
         target = match.group("target")
+        action = match.group("action")
         os_config = profile["activator"]
-        if target in os_config:
+        if target not in os_config:
+            #target not recognised
+            mic.say("I'm sorry. Target operating system %s is not recognised." % target)
+            return # break
+        if action == "activate":
             mic.say("Activating %s." % target)
             mac = os_config[target]["mac"]
             wol.send_magic_packet(mac)
             mic.say("Magic packet sent to %s host" % target)
-        else:
-            #target not recognised
-            mic.say("I'm sorry. Target operating system %s is not recognised." % target)
+        elif action == "deactivate":
+            mic.say("Deactivating %s." % target)
+            if target == "windows":
+                return
+            else:
+                passwd = os_config[target]["passwd"]
+                host = os_config[target]["host"]
+                out = call(["ssh", "pi@%s" % host, "sudo", "poweroff"])
+                if out == 0:
+                    mic.say("%s deactivated successfully!" % target)
+
     else:
         mic.say("I'm sorry I did not catch your last command. Please try again.")
 
@@ -42,4 +56,4 @@ def isValid(text):
         Arguments:
         text -- user-input, typically transcribed speech
     """
-    return bool(re.search(r'\b(activate\ (ubuntu|fedora|windows))\b', text, re.IGNORECASE))
+    return bool(re.search(r"\b(activate\ (ubuntu|fedora|windows))\b", text, re.IGNORECASE))
