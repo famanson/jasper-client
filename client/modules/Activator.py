6eeb067b14d3c649e76b1681d8a515b92d164efd
Son Pham <me@famanson.com>
2014-08-17 14:01:56 +0100

    Use node command to run lt

--- client/modules/Activator.py
+++ client/modules/Activator.py
@@ -61,7 +61,7 @@ def handle(text, mic, profile):
                     write(ser, "tunnel")
                     mic.say("Activating tunnel")
                     rnd_suffix = str(randint(1000,9999))
-                    commands.getoutput("lt --port 80 --subdomain %s &" % rnd_suffix)
+                    commands.getoutput("node /usr/local/bin/lt --port 80 --subdomain famanson%s &" % rnd_suffix)
                     mic.say("Your suffix is %s" % rnd_suffix)
                 else:
                     mic.say("Activating %s." % target)
